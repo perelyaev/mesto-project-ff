67ffab3f7c818cb9bbf64401700020964c39f678
perelyaev <perelyaev@gmail.com>
2023-12-20 21:18:01 +0800

    На ревью (20.12.2023)

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
+function addCard(link, name) {
+  const cardItem = cardTemplate.content.cloneNode(true);
+  cardItem.querySelector('.card__image').src = link;
+  cardItem.querySelector('.card__title').textContent = name;
+  const cardDeleteButton = cardItem.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', event => {
+    deleteCard(event);
+  });
+  cardList.append(cardItem);
+};
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const listItem = event.target.closest(':not(button)');
+  listItem.remove();
+}; 
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  addCard(card.link, card.name)
+});
